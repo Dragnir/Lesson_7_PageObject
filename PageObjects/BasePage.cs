df7a875a8e288ac99a17a67e7478545a6357b421
Vadzim Kuryan <Vadzim_Kuryan@epam.com>
2023-01-30 13:27:23 +0400

    Add new classes.

---
+++ PageObjects/BasePage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7_PageObject.PageObjects
+{
+    class BasePage
+    {
+        protected By _titleLocator;
+        protected string _title;
+        public static string _titleForm;
+
+        protected BasePage(By TitleLocator, string title)
+        {
+            _titleLocator = TitleLocator;
+            _title = _titleForm = title;
+            AssertIsOpen();
+        }
+
+        public void AssertIsOpen()
+        {
+           
+        }
+    }
+}
